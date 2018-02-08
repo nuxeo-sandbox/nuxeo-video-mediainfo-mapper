4447b924d4b4a2f0c828041230b9d8b79f81cbec
Maxime HILAIRE <mhilaire@nuxeo.com>
2018-02-07 17:11:28 -0700

    Revert "Update default video info in worker to avoid bug for watermarking"
    This reverts commit 257b52bd16f9abe48cd5182f9b72eb3a6c443d8e.

--- nuxeo-video-mediainfo-mapper-core/src/main/java/org/nuxeo/labs/video/mediainfo/mapper/VideoInfoWorker.java
+++ nuxeo-video-mediainfo-mapper-core/src/main/java/org/nuxeo/labs/video/mediainfo/mapper/VideoInfoWorker.java
@@ -1,29 +1,22 @@
 package org.nuxeo.labs.video.mediainfo.mapper;
 
-import static org.nuxeo.ecm.platform.video.VideoConstants.STORYBOARD_PROPERTY;
-import static org.nuxeo.ecm.platform.video.VideoConstants.TRANSCODED_VIDEOS_PROPERTY;
-import static org.nuxeo.ecm.platform.video.VideoConstants.VIDEO_CHANGED_EVENT;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.automation.AutomationService;
 import org.nuxeo.ecm.automation.OperationChain;
 import org.nuxeo.ecm.automation.OperationContext;
 import org.nuxeo.ecm.automation.OperationException;
-import org.nuxeo.ecm.core.api.Blob;
-import org.nuxeo.ecm.core.api.CoreSession;
-import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.IdRef;
-import org.nuxeo.ecm.core.api.NuxeoException;
+import org.nuxeo.ecm.core.api.*;
 import org.nuxeo.ecm.core.api.model.Property;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventService;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.ecm.core.event.impl.EventContextImpl;
 import org.nuxeo.ecm.core.work.AbstractWork;
-import org.nuxeo.ecm.platform.video.VideoHelper;
 import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.transaction.TransactionHelper;
+
+import static org.nuxeo.ecm.platform.video.VideoConstants.*;
 
 public class VideoInfoWorker extends AbstractWork {
 
@@ -70,7 +63,7 @@ public class VideoInfoWorker extends AbstractWork {
 
         EventContextImpl evctx = new DocumentEventContext(session, session.getPrincipal(), doc);
         Event eventToSend = evctx.newEvent(VIDEO_CHANGED_EVENT);
-        EventService eventService = Framework.getService(EventService.class);
+        EventService eventService = Framework.getLocalService(EventService.class);
         eventService.fireEvent(eventToSend);
 
         setStatus("Done");
@@ -91,10 +84,6 @@ public class VideoInfoWorker extends AbstractWork {
         chain.add("javascript.MediaInfoMapping");
         try {
             doc = (DocumentModel) as.run(octx, chain);
-            Property origVideoProperty = doc.getProperty("file:content");
-            Blob video = (Blob) origVideoProperty.getValue();
-            
-            VideoHelper.updateVideoInfo(doc, video);
             session.saveDocument(doc);
         } catch (OperationException e) {
             log.error(e);
