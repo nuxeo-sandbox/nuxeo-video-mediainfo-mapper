2bb6a151bf884326f6cbeced31eeada88d28bfa0
Michaël Vachette <mvachette@nuxeo.com>
2018-02-21 16:08:14 -0500

    Filter empty blob in listener

--- nuxeo-video-mediainfo-mapper-core/src/main/java/org/nuxeo/labs/video/mediainfo/mapper/CustomVideoChangedListener.java
+++ nuxeo-video-mediainfo-mapper-core/src/main/java/org/nuxeo/labs/video/mediainfo/mapper/CustomVideoChangedListener.java
@@ -2,6 +2,7 @@ package org.nuxeo.labs.video.mediainfo.mapper;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.model.Property;
 import org.nuxeo.ecm.core.event.Event;
@@ -11,9 +12,10 @@ import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.ecm.core.work.api.WorkManager;
 import org.nuxeo.runtime.api.Framework;
 
+import static org.nuxeo.ecm.core.api.event.DocumentEventTypes.BEFORE_DOC_UPDATE;
 import static org.nuxeo.ecm.core.api.event.DocumentEventTypes.DOCUMENT_CREATED;
-import static org.nuxeo.ecm.platform.video.VideoConstants.CTX_FORCE_INFORMATIONS_GENERATION;
-import static org.nuxeo.ecm.platform.video.VideoConstants.HAS_VIDEO_PREVIEW_FACET;
+import static org.nuxeo.ecm.platform.picture.api.ImagingDocumentConstants.PICTURE_VIEWS_PROPERTY;
+import static org.nuxeo.ecm.platform.video.VideoConstants.*;
 
 public class CustomVideoChangedListener implements EventListener {
 
@@ -31,9 +33,21 @@ public class CustomVideoChangedListener implements EventListener {
             boolean forceGeneration = Boolean.TRUE.equals(doc.getContextData(CTX_FORCE_INFORMATIONS_GENERATION));
             Property origVideoProperty = doc.getProperty("file:content");
             if (forceGeneration || DOCUMENT_CREATED.equals(event.getName()) || origVideoProperty.isDirty()) {
-                VideoInfoWorker work = new VideoInfoWorker(doc.getRepositoryName(), doc.getId());
-                WorkManager workManager = Framework.getLocalService(WorkManager.class);
-                workManager.schedule(work, WorkManager.Scheduling.IF_NOT_SCHEDULED, true);
+
+                Blob video = (Blob) origVideoProperty.getValue();
+
+                if (BEFORE_DOC_UPDATE.equals(event.getName())) {
+                    doc.setPropertyValue(TRANSCODED_VIDEOS_PROPERTY, null);
+                    doc.setPropertyValue(STORYBOARD_PROPERTY, null);
+                    doc.setPropertyValue(PICTURE_VIEWS_PROPERTY, null);
+                }
+
+                // only trigger the event if we really have a video
+                if (video != null) {
+                    VideoInfoWorker work = new VideoInfoWorker(doc.getRepositoryName(), doc.getId());
+                    WorkManager workManager = Framework.getService(WorkManager.class);
+                    workManager.schedule(work, WorkManager.Scheduling.IF_NOT_SCHEDULED, true);
+                }
             }
         }
     }
